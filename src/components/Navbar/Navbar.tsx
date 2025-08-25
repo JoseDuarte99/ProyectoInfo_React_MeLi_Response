28a23fdc035401c152332f249ea6e61c195e723e
JoseDuarte <estebnjose@gmail.com>
2025-08-25 17:52:51 -0300

    Trabajando en el Header

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import style from "./navbar.module.css"
+import style from "./Navbar.module.css"
 
 function Navbar() {
 
     const [menuValue, setMenuValue] = useState(false); 
-
-
 
     return (
         <header className={style.header}>
@@ -17,18 +15,16 @@ function Navbar() {
                 <div className={style.buttons}>Buttons</div>
                 <div className={style.login}>Login</div>
 
-                <nav className={style.manu}>
-                    <input type="checkbox" id="menu" checked={menuValue} className={style.menuInput} onClick={() => setMenuValue(!menuValue)} />   
+                <nav className={style.menu}>
+                    <input type="checkbox" id="menu" checked={menuValue} className={style.menuInput} onChange={() => setMenuValue(!menuValue)} />   
                     <label htmlFor="menu" className={style.activeMenu}>
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  
+                        <svg  xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor" viewBox="0 0 24 24" >
-                        <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z"></path>
+                            <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z"></path>
                         </svg>
                     </label>
-
                     <ul className={style.itemsMenu}>
-                                            <input type="checkbox" id="closedMenu" checked={menuValue} className={style.closedMenu} onClick={() => setMenuValue(!menuValue)}/>
-
+                        <input type="checkbox" id="closedMenu" checked={menuValue} className={style.closedMenu} onChange={() => setMenuValue(!menuValue)}/>
                         <li><a href="#">Advertising</a></li>
                         <li><a href="#">Location</a></li>
                         <li><a href="#">Buttons</a></li>
