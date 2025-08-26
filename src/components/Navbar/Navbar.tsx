174efe3582385c6b25ce5b07238ad8a11b2da686
JoseDuarte <estebnjose@gmail.com>
2025-08-26 17:38:04 -0300

    Avanzando con el Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import style from "./Navbar.module.css"
 
+import imgLogo from "../../assets/LogoMeLi.svg"
+import imgLogoSmall from "../../assets/LogoMeLi-Small.png"
+import imgShippingFree from "../../assets/EnvioGratis.webp";
+
+
 function Navbar() {
 
     const [menuValue, setMenuValue] = useState(false); 
@@ -8,9 +13,12 @@ function Navbar() {
     return (
         <header className={style.header}>
             <div className={style.container}>
-                <div className={style.logo}>Logo</div>
+                <img src={imgLogo} alt="Logo" className={style.logo}/>
+                <img src={imgLogoSmall} alt="Logo" className={style.logoSmall} />
                 <div className={style.search}>Search</div>
-                <div className={style.advertising}>Advertising</div>
+                <div className={style.advertising}>
+                    <img src={imgShippingFree} alt="Logo de Mercado Libre"  />
+                </div>
                 <div className={style.location}>Location</div>
                 <div className={style.buttons}>Buttons</div>
                 <div className={style.login}>Login</div>
@@ -18,19 +26,17 @@ function Navbar() {
                 <nav className={style.menu}>
                     <input type="checkbox" id="menu" checked={menuValue} className={style.menuInput} onChange={() => setMenuValue(!menuValue)} />   
                     <label htmlFor="menu" className={style.activeMenu}>
-                        <svg  xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor" viewBox="0 0 24 24" >
-                            <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z"></path>
-                        </svg>
-                    </label>
-                    <ul className={style.itemsMenu}>
-                        <input type="checkbox" id="closedMenu" checked={menuValue} className={style.menuInput} onChange={() => setMenuValue(!menuValue)}/>
-                        <label htmlFor="closedMenu" className={style.closedMenu}> 
-                            <svg xmlns="http://www.w3.org/2000/svg" 
+                        { menuValue   
+                        ?   <svg xmlns="http://www.w3.org/2000/svg" 
                                 fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M6 6l12 12M6 18L18 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-                            </svg>
-                        </label>
+                            </svg>                 
+                        :   <svg  xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 24 24" >
+                                <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z" ></path>
+                            </svg>}
+                    </label>
+                    <ul className={style.itemsMenu}>
                         <li><a href="#">Advertising</a></li>
                         <li><a href="#">Location</a></li>
                         <li><a href="#">Buttons</a></li>
