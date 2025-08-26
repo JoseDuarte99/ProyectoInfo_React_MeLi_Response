cf4cef9fbd92b4214086245e7678fd4a2c98e043
JoseDuarte <estebnjosed@gmail.com>
2025-08-25 22:30:30 -0300

    Continuando con Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,7 +24,13 @@ function Navbar() {
                         </svg>
                     </label>
                     <ul className={style.itemsMenu}>
-                        <input type="checkbox" id="closedMenu" checked={menuValue} className={style.closedMenu} onChange={() => setMenuValue(!menuValue)}/>
+                        <input type="checkbox" id="closedMenu" checked={menuValue} className={style.menuInput} onChange={() => setMenuValue(!menuValue)}/>
+                        <label htmlFor="closedMenu" className={style.closedMenu}> 
+                            <svg xmlns="http://www.w3.org/2000/svg" 
+                                fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M6 6l12 12M6 18L18 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+                            </svg>
+                        </label>
                         <li><a href="#">Advertising</a></li>
                         <li><a href="#">Location</a></li>
                         <li><a href="#">Buttons</a></li>
