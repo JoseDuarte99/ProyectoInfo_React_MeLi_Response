2ffc5f904428e09c38960fa0f963bbe97750b756
JoseDuarte <estebnjose@gmail.com>
2025-09-05 17:22:50 -0300

    Menu desplegable

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,8 +1,9 @@
 // Import Style
 import style from "./DropdownMenu.module.css"
 
-// Import IMG
+// Import IMG and SVG
 import ProfileNone from "../../../public/SinPerfil.jpg"
+import { HomeSvg, OfferSvg, PlaySvg, HistorySvg, HeadsetSvg, SuperMarketSvg } from "./DropdownMenuIcons"
 
 
 function DropdownMenu() {
@@ -16,6 +17,29 @@ function DropdownMenu() {
                     <button> Ingresá </button>
                     <button> Creá tu cuenta </button>
                 </div>
+            </div>
+            <div className={style.options}>
+                <ul>
+                    <li>{HomeSvg}<a href="">Inicio</a></li>
+                    <li>{OfferSvg}<a href="">Ofertas</a></li>
+                    <li>{PlaySvg}<a href="">Mercado Play</a><span className={style.buttonMercadoPlay}>GRATIS</span></li>
+                    <li>{HistorySvg}<a href="">Historial</a></li>
+                    <li>{HeadsetSvg}<a href="">Ayuda</a></li>
+                </ul>
+                <ul>
+                    <li>{SuperMarketSvg}<a href="">Supermercado</a></li>
+                    <li>{OfferSvg}<a href="">Moda</a></li>
+                    <li>{PlaySvg}<a href="">Mas vendidos</a><span className={style.buttonBestSelling}>NUEVO</span></li>
+                    <li>{HistorySvg}<a href="">Tiendas oficiales</a></li>
+                    <li>{HeadsetSvg}<a href="">Categorías</a></li>
+                </ul>
+                <ul>
+                    <li>{HistorySvg}<a href="">Resumen</a></li>
+                    <li>{HeadsetSvg}<a href="">Vender</a></li>
+                </ul>
+                <ul>
+                    <li>{HeadsetSvg}<a href="">¡Comprá y vendé con la app!</a></li>
+                </ul>
             </div>
         </>
     )
